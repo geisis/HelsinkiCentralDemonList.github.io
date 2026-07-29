d0724db134d488606bdaee62ce208bdc9061c92d
geisis <78940095+geisis@users.noreply.github.com>
2026-07-28 23:31:54 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+fetch("demons.json")
+.then(response => response.json())
+.then(demons => {
 
+
+    let table = document.getElementById("demon-table");
+
+
+    if(table){
+
+        demons.forEach(demon => {
+
+            let row = document.createElement("tr");
+
+            row.innerHTML = `
+            <td>${demon.position}</td>
+            <td>${demon.name}</td>
+            <td>${demon.creator}</td>
+            <td>${demon.verifier}</td>
+            <td>${demon.points}</td>
+            `;
+
+            table.appendChild(row);
+
+        });
+
+    }
+
+
+
+    let top = document.getElementById("top-demons");
+
+
+    if(top){
+
+        demons.slice(0,3).forEach(demon=>{
+
+            let card=document.createElement("div");
+
+            card.className="card";
+
+            card.innerHTML=`
+            <h3>#${demon.position} ${demon.name}</h3>
+            <p>${demon.points} points</p>
+            `;
+
+            top.appendChild(card);
+
+        });
+
+    }
+
+
+
+});
